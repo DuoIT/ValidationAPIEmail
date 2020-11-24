ab073b0ff5d809f7ec9dca894ebe59a471e049fa
DuoIT <nguyenthuyduong72018@gmail.com>
2020-11-24 16:32:18 +0700

    my first commit

---
+++ expenses/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from authentication.models import User
+# Create your models here.
+
+
+class Expense(models.Model):
+
+    CATEGORY_OPTIONS = [
+        ('ONLINE_SERVICES', 'ONLINE_SERVICES'),
+        ('TRAVEL', 'TRAVEL'),
+        ('FOOD', 'FOOD'),
+        ('RENT', 'RENT'),
+        ('OTHERS', 'OTHERS')
+    ]
+
+    category = models.CharField(max_length=255, choices=CATEGORY_OPTIONS)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, max_length=255)
+    description = models.TextField()
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    date = models.DateField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
